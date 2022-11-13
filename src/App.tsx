fa3886c6aaeb786298d3af7e405c9c9b797c5b54
DESKTOP-O6FJ9MB\jaros <jaroslaw.kurdzieko@gmail.com>
2022-11-13 12:28:21 +0100

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import MenuIcon from "@mui/icons-material/Menu";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import Badge from "@mui/material/Badge";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import { ThemeProvider } from "@mui/material/styles";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import * as React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { ExerciseBuilder } from "./features/exercise-builder/ExerciseBuilder";
-import { Home } from "./features/home/Home";
-import { mainListItems, secondaryListItems } from "./listItems";
-import { AppBar } from "./shared/components/app-bar/AppBar";
-import { Drawer } from "./shared/components/drawer/Drawer";
-import { mdTheme } from "./theme";
-
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import MenuIcon from '@mui/icons-material/Menu'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import Badge from '@mui/material/Badge'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
+import List from '@mui/material/List'
+import { ThemeProvider } from '@mui/material/styles'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import * as React from 'react'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { ExerciseBuilder } from './features/exercise-builder/ExerciseBuilder'
+import { Home } from './features/home/Home'
+import { TrainingBuilder } from './features/training-builder/TrainingBuilder'
+import { AppBar } from './shared/components/app-bar/AppBar'
+import { Drawer } from './shared/components/drawer/Drawer'
+import { MenuItems, SecondaryMenuItems } from './shared/components/menu-items/MenuItems'
+import { mdTheme } from './theme'
 function App() {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(true)
   const toggleDrawer = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
   return (
     <ThemeProvider theme={mdTheme}>
       <Router>
-        <Box sx={{ display: "flex" }}>
+        <Box sx={{ display: 'flex' }}>
           <CssBaseline />
           <AppBar position="absolute" open={open}>
             <Toolbar
               sx={{
-                pr: "24px", // keep right padding when drawer closed
+                pr: '24px', // keep right padding when drawer closed
               }}
             >
               <IconButton
@@ -42,21 +43,19 @@ function App() {
                 aria-label="open drawer"
                 onClick={toggleDrawer}
                 sx={{
-                  marginRight: "36px",
-                  ...(open && { display: "none" }),
+                  marginRight: '36px',
+                  ...(open && { display: 'none' }),
                 }}
               >
                 <MenuIcon />
               </IconButton>
-              <Typography
-                component="h1"
-                variant="h6"
-                color="inherit"
-                noWrap
-                sx={{ flexGrow: 1 }}
-              >
-                Dashboard
+
+              <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+                <Link color="inherit" underline="none" href="#">
+                  Sportcademy
+                </Link>
               </Typography>
+
               <IconButton color="inherit">
                 <Badge badgeContent={4} color="secondary">
                   <NotificationsIcon />
@@ -67,9 +66,9 @@ function App() {
           <Drawer variant="permanent" open={open}>
             <Toolbar
               sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "flex-end",
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
                 px: [1],
               }}
             >
@@ -79,28 +78,26 @@ function App() {
             </Toolbar>
             <Divider />
             <List component="nav">
-              {mainListItems}
+              {MenuItems}
               <Divider sx={{ my: 1 }} />
-              {secondaryListItems}
+              {SecondaryMenuItems}
             </List>
           </Drawer>
           <Box
             component="main"
             sx={{
               backgroundColor: (theme) =>
-                theme.palette.mode === "light"
-                  ? theme.palette.grey[100]
-                  : theme.palette.grey[900],
+                theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
               flexGrow: 1,
-              height: "100vh",
-              overflow: "auto",
+              height: '100vh',
+              overflow: 'auto',
             }}
           >
             <Toolbar />
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/exercise-builder" element={<ExerciseBuilder />} />
-              <Route path="/conspect-builder" element={<ExerciseBuilder />} />
+              <Route path="/training-builder" element={<TrainingBuilder />} />
 
               <Route
                 path="*"
@@ -115,6 +112,6 @@ function App() {
         </Box>
       </Router>
     </ThemeProvider>
-  );
+  )
 }
-export default App;
+export default App
--- src/features/exercise-builder/ExerciseBuilder.tsx
+++ src/features/exercise-builder/ExerciseBuilder.tsx
@@ -1,69 +1,103 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './exerciseBuilderSlice';
-import styles from './ExerciseBuilder.module.css';
+import { Container, Grid, Paper } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { selectCount } from "./exerciseBuilderSlice";
+import { ToolBar } from "./toolbar/ToolBar";
 
 export function ExerciseBuilder() {
   const count = useAppSelector(selectCount);
   const dispatch = useAppDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   const incrementValue = Number(incrementAmount) || 0;
 
   return (
-    <div>
-      Exercise builder
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
+    <>
+      <ToolBar />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            >
+              {/* <Chart /> */}
+            </Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: "flex",
+                flexDirection: "column",
+                height: 240,
+              }}
+            >
+              {/* <Deposits /> */}
+            </Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+              {/* <Orders /> */}
+            </Paper>
+          </Grid>
+        </Grid>
+        {/* <Copyright sx={{ pt: 4 }} /> */}
+      </Container>
+    </>
+    // <div>
+    //   Exercise builder
+    //   <div className={styles.row}>
+    //     <button
+    //       className={styles.button}
+    //       aria-label="Decrement value"
+    //       onClick={() => dispatch(decrement())}
+    //     >
+    //       -
+    //     </button>
+    //     <span className={styles.value}>{count}</span>
+    //     <button
+    //       className={styles.button}
+    //       aria-label="Increment value"
+    //       onClick={() => dispatch(increment())}
+    //     >
+    //       +
+    //     </button>
+    //   </div>
+    //   <div className={styles.row}>
+    //     <input
+    //       className={styles.textbox}
+    //       aria-label="Set increment amount"
+    //       value={incrementAmount}
+    //       onChange={(e) => setIncrementAmount(e.target.value)}
+    //     />
+    //     <button
+    //       className={styles.button}
+    //       onClick={() => dispatch(incrementByAmount(incrementValue))}
+    //     >
+    //       Add Amount
+    //     </button>
+    //     <button
+    //       className={styles.asyncButton}
+    //       onClick={() => dispatch(incrementAsync(incrementValue))}
+    //     >
+    //       Add Async
+    //     </button>
+    //     <button
+    //       className={styles.button}
+    //       onClick={() => dispatch(incrementIfOdd(incrementValue))}
+    //     >
+    //       Add If Odd
+    //     </button>
+    //   </div>
+    // </div>
   );
 }
