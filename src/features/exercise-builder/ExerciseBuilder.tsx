c804725cef2e67efca837bd676d24d7c84cb28fe
DESKTOP-O6FJ9MB\jaros <jaroslaw.kurdzieko@gmail.com>
2022-12-31 14:47:05 +0100

    aff splitter

--- src/features/exercise-builder/ExerciseBuilder.tsx
+++ src/features/exercise-builder/ExerciseBuilder.tsx
@@ -1,59 +1,16 @@
-import { Box, Container, Grid, Paper } from '@mui/material'
 import Split from 'react-split'
 import { CodeEditor } from './code-editor/CodeEditor'
+import { ExerciseActivity } from './exercise-activity/ExerciseActivity'
+import { ExerciseVisualizer } from './exercise-visualizer/ExerciseVisualizer'
 import './ExerciseBuilder.css'
-import { ToolBar } from './toolbar/ToolBar'
-import { Visualizer } from './visualizer/Visualizer'
 export function ExerciseBuilder() {
   return (
-    // <Split
-    //   className="flex"
-    //   sizes={[20, 60, 20]}
-    //   direction="horizontal"
-    //   style={{ height: `calc(100vh - 4rem)` }}
-    // >
-    //   <div className="bg-gray-300">1</div>
-    //   <div className="bg-gray-300">
-    //     <ToolBar />
-    //   </div>
-    //   <div className="bg-gray-300">3</div>
-    // </Split>
-    <Split direction="vertical" style={{ height: `calc(100vh - 4rem)` }}>
-      <Split className="superflex" >
-      <div className="bluecolor" />
-      <div className="redcolor" />
-      <div className="bluecolor" />
+    <Split direction="vertical" style={{ height: `calc(100vh - 4rem)` }} sizes={[75, 25]}>
+      <Split className="superflex" sizes={[75, 25]}>
+        <ExerciseVisualizer />
+        <ExerciseActivity/>
       </Split>
-      <div className="redcolor" />
+      <CodeEditor />
     </Split>
-  )
-  return (
-    <Box
-      component="main"
-      sx={{
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[900],
-        flexGrow: 1,
-        height: '90vh',
-        overflow: 'auto',
-      }}
-    >
-      <ToolBar />
-      <Container maxWidth={false} sx={{ mt: 2, mb: 2 }}>
-        <Grid container spacing={1}>
-          <Grid item xs={12}>
-            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: '43vh' }}>
-              <Visualizer />
-            </Paper>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', height: '35vh' }}>
-              <CodeEditor />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
   )
 }
